f5a7b643e339958a011c5d21847794b964956397
Visa Keskinen <visa.keskinen@gmail.com>
2023-02-28 17:42:40 +0200

    Remove hidden text element

--- src/App.js
+++ src/App.js
@@ -6,10 +6,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Welcome to Bus Tracker</h1>
-        <p>
-          Use this app to see live location of Tampere public transit vehicles
-          (bus and tram)
-        </p>
       </header>
       <LeafletMap />
     </div>
