956153cb6bb4440eef8d6f3808a70abc61fda2da
Visa Keskinen <visa.keskinen@gmail.com>
2023-02-28 18:16:16 +0200

    Remove unused layers

--- src/components/LeafletMap.jsx
+++ src/components/LeafletMap.jsx
@@ -28,19 +28,6 @@ export default function LeafletComponent() {
               url="https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw"
             />
           </LayersControl.BaseLayer>
-          <LayersControl.Overlay name="OpenTopoMap" checked={false}>
-            <TileLayer
-              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png"
-              opacity={0.5}
-            />
-          </LayersControl.Overlay>
-          <LayersControl.Overlay name="Transit" checked={false}>
-            <TileLayer
-              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/layers=T/{z}/{x}/{y}.png"
-            />
-          </LayersControl.Overlay>
         </LayersControl>
         <VehicleMarkers />
         <StopPointMarkers />
