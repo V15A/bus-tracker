e71dfd0f1e2b2337dda12c93d7ee722102485da6
Visa Keskinen <visa.keskinen@gmail.com>
2025-02-26 08:46:05 +0200

    Update url and move api key to env for background map

--- src/components/LeafletMap.jsx
+++ src/components/LeafletMap.jsx
@@ -7,11 +7,11 @@ import { StopPointMarkers } from "./StopPointMarkers";
  */
 export default function LeafletComponent() {
   return (
-    <div class="leaflet-container">
+    <div className="leaflet-container">
       <MapContainer center={[61.495, 23.775]} zoom={15} minZoom={10}>
         <TileLayer
           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://dev-api.digitransit.fi/map/v2/hsl-map-en-256/{z}/{x}/{y}.png?digitransit-subscription-key=c07c7f1e3a13429d996d5aa54b91ebd8"
+          url={`https://cdn.digitransit.fi/map/v3/hsl-map-en-256/{z}/{x}/{y}.png?digitransit-subscription-key=${process.env.REACT_APP_DIGITRANSIT_API_KEY}`}
         />
 
         <VehicleMarkers />
